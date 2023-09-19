2d9ea4e632b5a483090fe755289d4575f06924bf
taylanunutmaz <taylanunutmaz@gmail.com>
2023-09-19 23:42:12 +0300

    remove dummy importer

--- src/utils/jsxComponentDescriptors.tsx
+++ src/utils/jsxComponentDescriptors.tsx
@@ -6,7 +6,6 @@ export const jsxComponentDescriptors: JsxComponentDescriptor[] = [
     kind: "flow", // 'text' for inline, 'flow' for block
     // the source field is used to construct the import statement at the top of the markdown document.
     // it won't be actually sourced.
-    source: "@/components/text",
     // Used to construc the property popover of the generic editor
     props: [{ name: "val", type: "string" }],
     // wether the component has children or not
